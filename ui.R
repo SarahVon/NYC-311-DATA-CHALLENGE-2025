6a3b5e40ef6aaa8ab699b5a9712ffd8234e28f12
SarahVon <sarahvonanderson@gmail.com>
2025-03-08 21:38:12 -0800

    creating the dashboardBody and putting Mia's ui code into the first tab slot, following the "About" section.

--- ui.R
+++ ui.R
@@ -10,3 +10,46 @@ sidebar = dashboardSidebar(
     menuItem("viz 4", tabName = "viz_4")
   )
 )
+
+# creating the body of the dashboard to hold the page content
+body = dashboardBody(
+  tabItems(
+    # about tab content
+    tabItem(tabName = "About",
+            fluidPage()
+    ),
+    
+    # first visualization tab content
+    tabItem(tabName = "viz_1",
+            fluidPage(
+              titlePanel("NYC 311 Complaints Dashboard"),
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("borough", "Select Borough:", 
+                              choices = borough_choices, 
+                              selected = borough_choices[1]) 
+                ),
+                mainPanel(
+                  leafletOutput("nyc_map", height = 400),  
+                  plotOutput("complaint_plot")
+                )
+              )
+            )
+    ),
+    
+    # second visualization tab content
+    tabItem(tabName = "viz_2",
+            fluidPage()
+    ),
+    
+    # third visualization tab content
+    tabItem(tabName = "viz_3",
+            fluidPage()
+    ),
+    
+    # fourth visualization tab content
+    tabItem(tabName = "viz_4",
+            fluidPage()
+    )
+  )
+)
