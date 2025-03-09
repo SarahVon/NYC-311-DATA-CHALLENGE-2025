62de3117715ef35c2594c4be3d8de6a886a5892b
SarahVon <sarahvonanderson@gmail.com>
2025-03-08 21:33:32 -0800

    loading libraries and data. Some lines are commented out for now due to some dropbox issues, but will be resolved soon. A local copy was being  used when this commit was made

--- global.R
+++ global.R
@@ -1,1 +1,21 @@
- 
+library(tidyverse)
+library(stringr)
+library(shiny)
+library(readr)
+library(leaflet)
+library(sf)
+library(tigris)  
+options(tigris_use_cache = TRUE)
+library(spData)
+data("us_states", package = "spData")
+
+# commenting the below out for now because my dropbox account was flagged again... 
+
+# # dropbox direct download link to access large file (+2gb)
+# dataset_url <- "https://www.dropbox.com/scl/fi/os1x9i7sx1io5ura5fjti/311_DATA.csv?rlkey=quw01tcso6lz3u3vtx7seoalc&st=634psn3v&dl=1"
+# 
+# # read data directly from Dropbox
+# data <- read_csv(dataset_url)
+
+# using local data for now
+data <- read_csv("311_DATA.csv")
